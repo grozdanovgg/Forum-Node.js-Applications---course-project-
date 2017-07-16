fd5966a6c668c6dd3e95822bc323381949f38bf5
Martin Kamenov <martykam@abv.bg>
2017-07-16 19:05:27 +0300

    Added register to TellUS(without passport yet).

--- Forum/app.js
+++ Forum/app.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const attach = require('./public/routes/app-route');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static(path.join(__dirname, './static')));
 app.use('/libs', express.static(path.join(__dirname, './node_modules')));
+app.use(bodyParser.urlencoded());
 
 attach(app);
 
@@ -15,7 +17,7 @@ attach(app);
 
 const db = require('./database/mongodb');
 const database = new db('mongodb://localhost/items-db');
-const record = {
+/*const record = {
     title: 'Why does it rain',
     author: 'Martin Kamenov',
 };
@@ -27,8 +29,6 @@ for (var i = 0; i < 23; i++) {
         author: 'Martin Kamenov',
     };
     database.insert('posts', record).then();
-}
-database.showAll('posts').then((posts) => console.log(posts));
-
+}*/
 
 app.listen(3001, () => console.log('Magic is running at 3001'));
--- Forum/public/routes/app-route.js
+++ Forum/public/routes/app-route.js
@@ -54,7 +54,41 @@ const attach = (app) => {
         .get('/register', (req, res) => {
             res.render('register');
         })
-        .post('/register', (req, res) => { res.send(req.body) });
+        .post('/register', (req, res) => {
+            const username = req.body.username;
+            const password = req.body.password;
+            const email = req.body.email;
+            const repeatpassword = req.body.password_confirm;
+            console.log(username);
+            console.log(password);
+            console.log(email);
+            console.log(repeatpassword);
+            if (!password || !username || !email || !repeatpassword) {
+                const message = 'All fields are required!';
+                res.render('register', { message });
+            }
+
+            if (password !== repeatpassword) {
+                const message = 'Passwords are not the same!';
+                res.render('register', { message });
+            }
+
+            db.find('users', { 'username': username }).then((users) => {
+                if (users.length > 0) {
+                    const message = 'There is user with this username!';
+                    res.render('register', { message });
+                }
+                const newUser = { username, password, email, repeatpassword };
+                db.insert('users', newUser).then(() => {
+                    const message = 'Successfully Registered';
+                    res.render('postRegister', { message });
+                }).catch(() => {
+                    const message = 'We are sorry, but there was a problem with registering...';
+                    res.render('postRegister', { message })
+                });
+
+            });
+        });
 
 
     app.use('/', router);
